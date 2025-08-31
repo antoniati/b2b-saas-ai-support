8866449c9bbe6805e922d2f8abb34bcea9483455
antoniati <antoniati.felipe@gmail.com>
2025-08-31 13:51:33 -0300

    chore(auth): Define versão fixa next-auth@5.0.0-beta.21 no package.json para evitar quebras futuras.

---
+++ shared/index.ts
@@ -0,0 +1,12 @@
+export * from './types/action.type';
+export * from './types/mails.type';
+
+export * from './lib/prisma';
+
+export * from './utils/action.utils';
+export * from './utils/validation.utils';
+
+export * from './constants/error.messages';
+export * from './constants/success.messages';
+
+export * from './errors/domain.errors';
